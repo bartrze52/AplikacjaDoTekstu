e0bc7de9e0555c71bfa4e26714862dde8a3302c6
trzesniewski.bartosz <trzesniewski.bartosz@STACJA105-05>
2024-02-19 15:12:28 +0100

    Dodaj pliki projektów.

---
+++ Essa/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Win32;
+using System.CodeDom.Compiler;
+using System.ComponentModel.Design;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace Essa
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            System.Windows.Input.ApplicationCommands.Close.InputGestures.Add(new KeyGesture(Key.Q, ModifierKeys.Control));
+            InitializeComponent();
+        }
+
+        private void CommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void CommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (MessageBox.Show("Czy na pewno zamknąć aplikację?", "Zamykanie", MessageBoxButton.OKCancel, MessageBoxImage.Warning) == MessageBoxResult.OK)
+                Application.Current.Shutdown();
+        }
+
+        private void CommandBinding_CanExecute_1(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void CommandBinding_Executed_1(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (menuZawijanie.IsChecked)
+                mojTekst.TextWrapping = TextWrapping.Wrap;
+            else
+                mojTekst.TextWrapping = TextWrapping.NoWrap;
+        }
+
+        private void CommandBinding_CanExecute_2(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (mojTekst.Text != string.Empty)
+                e.CanExecute = true;
+            else
+                e.CanExecute = false;
+        }
+
+        private void CommandBinding_Executed_2(object sender, ExecutedRoutedEventArgs e)
+        {
+            mojTekst.FontSize += 5;
+        }
+
+        private void CommandBinding_CanExecute_3(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (mojTekst.Text != string.Empty)
+                e.CanExecute = true;
+        }
+
+        private void CommandBinding_Executed_3(object sender, ExecutedRoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                File.WriteAllText(saveFileDialog.FileName, mojTekst.Text);
+            }
+        }
+    }
+}
---
+++ Essa/MojeKomendy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Essa
+{
+    public static class MojeKomendy
+    {
+        public static readonly RoutedUICommand Zawijaj = new RoutedUICommand("Zawijaj wiersze", "Zawijanie",
+            typeof(MojeKomendy),
+            new InputGestureCollection()
+            {
+                new KeyGesture(Key.R, ModifierKeys.Control)
+            });
+
+        public static readonly RoutedUICommand Powieksz = new RoutedUICommand("Powiększ tekst", "Powiększanie",
+            typeof(MojeKomendy),
+            new InputGestureCollection()
+            {
+                new KeyGesture(Key.P, ModifierKeys.Control)
+            });
+    }
+}
